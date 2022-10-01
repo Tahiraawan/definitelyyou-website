f7e216d350c13df5db2d7db6e527026fcd6f217c
Tahira-Malik <tahiramalik6644@gmail.com>
2022-10-01 22:39:39 +0500

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div> this is Header</div>
-  )
+    <header>
+      <div className="info">
+        <span className="email">definitlyyou@gmail.com</span>
+        <span className="number">0304-34343434</span>
+      </div>
+      <nav>
+        <div className="logo">
+          <span>LOGO</span>
+        </div>
+        <div className="menuitems">
+          <Link to={''}>Home</Link>
+          <Link to={''}>About</Link>
+          <Link to={''}>Shalwar Kameez</Link>
+          <Link to={''}>Frocks</Link>
+          <Link to={''}>Fancy dress</Link>
+          <Link to={''}>unstitched fabric</Link>
+        </div>
+        <div className="profile">
+          <span>cart</span>
+          <span>
+            <select name="profile" id="profile">
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
