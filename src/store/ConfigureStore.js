d38f78af542506f4ce5fa421b7e8670d0187782e
Tahira-Malik <tahiramalik6644@gmail.com>
2022-10-11 13:49:27 +0500

    setting store

--- src/store/ConfigureStore.js
+++
@@ -1,9 +0,0 @@
-import userSliceReducer from './userSlice'
-import { configureStore } from "@reduxjs/toolkit";
-//create store..
-export const store=configureStore({
-reducer:{
-    //key wohi dain h jo userslice m daini h 
-user: userSliceReducer
-}
-});
--- src/store/UserSlice.js
+++
@@ -1,6 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-//1 create initial state
-const initialState={
-    first_name:"",
-    last_name:"",
-    isLoggedIn:false}
